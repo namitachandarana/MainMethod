4972de01a90b0ad392f1a32af9eb291021e936d7
MSALM\NamitaCSD <Namita@sqs.com>
2019-07-03 14:18:13 +0100

    Adding some code for learning about the comments tutorial

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,10 +3,21 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println ("args-size= " + args.length);
+        System.out.println("args-size= " + args.length);
 
-        for (int i = 0; i < args.length; i++){
+        for (int i = 0; i < args.length; i++) {
             System.out.println("args[" + i + "]=" + args[i]);
         }
+
+    }
+
+    /**
+     *
+     * @param a - is an integer type
+     * @param b - is an inter type too
+     * @return returns the sum of integers
+     */
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
